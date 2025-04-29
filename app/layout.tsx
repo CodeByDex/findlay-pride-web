314f79f846566efd2e368b36edb9c5e99261c477
CodeByDex <119005046+CodeByDex@users.noreply.github.com>
2025-04-29 15:55:29 -0400

    moved Findlay Pride to header and added site logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="p-4 flex items-center">
+            <Image src="/PrideF.png" alt={"the letter f stylized like a pride flag"} width={50} height={50} className="rounded-full" />
+            <div className="text-lg ml-5">
+              Findlay Pride
+            </div>
+        </header>
         {children}
+        <footer>
+
+        </footer>
       </body>
     </html>
   );
