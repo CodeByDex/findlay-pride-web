1da37ada5085a5cb45453b873c6f3d83096be45b
Ryan <119005046+CodeByDex@users.noreply.github.com>
2025-05-08 12:21:58 -0400

    updated sample events, adjusted styling based on larger content

--- app/sampleevent/page.tsx
+++ app/sampleevent/page.tsx
@@ -3,28 +3,36 @@ import { EventComponent } from "@/component/event"
 
 const events: Event[] = [
     {
-        name: "Sample Event",
-        description: "This event goes all month long.",
-        startDate: new Date(2025, 5, 1, 0, 0),
-        endDate: new Date(2025, 5, 30, 23, 59),
-        location: "Sample Location",
-        organizer: "Sample Organizer",
-        organizerURI: "https://example.com/organizer",
-        eventURI: "https://example.com/event",
-        tags: ["sample", "event"],
+        name: "Pride Weekend Celebration",
+        description: "Mark your calendars for Pride Weekend in Findlay! Join us June 6–8 for events with Findlay Brewing Co. and False Chord Brewing! Stay tuned and get ready to celebrate!",
+        startDate: new Date(2025, 5, 6, 0, 0),
+        endDate: new Date(2025, 5, 8, 23, 59),
+        location: "Findlay Brewing Company and False Chord",
+        organizer: "LGBTQ+ Spectrum of Findlay",
+        organizerURI: "facebook.com/lgbtqSpectrumofFindlay",
+        tags: [],
         imageURI: "/PrideF.png",
         imageAlt: "Sample Event Image",
     },
     {
-        name: "Sample Event2",
-        description: "This event goes for a few hours.",
-        startDate: new Date(2025, 5, 7,12, 0),
-        endDate: new Date(2025, 5, 7, 17, 0),
-        location: "Sample Location",
-        organizer: "Sample Organizer",
-        organizerURI: "https://example.com/organizer",
-        eventURI: "https://example.com/event",
-        tags: ["sample", "event"],
+        name: "Pride Month Drink Special",
+        description: "Stop in all month long to check out our drink specials for Pride Month!",
+        startDate: new Date(2025, 5, 1,0, 0),
+        endDate: new Date(2025, 5, 30, 23, 5),
+        location: "Wine Merchant",
+        organizer: "Wine Merchant",
+        organizerURI: "facebook.com/findlaywinemerchant",
+        imageURI: "/PrideF.png",
+        imageAlt: "Sample Event Image",
+    },
+    {
+        name: "Pride Month Coffee Special",
+        description: "Check out Amici for fun drink specials, check back soon for more details!",
+        startDate: new Date(2025, 5, 1,0, 0),
+        endDate: new Date(2025, 5, 30, 23, 5),
+        location: "Coffee Amici",
+        organizer: "Coffee Amici",
+        organizerURI: "facebook.com/coffeeamici",
         imageURI: "/PrideF.png",
         imageAlt: "Sample Event Image",
     },
@@ -33,7 +41,13 @@ const events: Event[] = [
 export default function EventPage() {
     return (
         <main className="sm:max-w-5/6 md:max-w-3/4">
-            {events.map((event, index) => (
+            {events.sort((a, b) => {
+                const startDiff = a.startDate.getDate() - b.startDate.getDate();
+
+                if (startDiff !== 0) return startDiff;
+
+                return a.endDate.getDate() - b.endDate.getDate();
+            }).map((event, index) => (
                 <EventComponent key={index} eventProps={event} />
             ))}
         </main>
