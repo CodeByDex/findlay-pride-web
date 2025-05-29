26c35f35117f9d8f60602f1b8a70069c3622b439
CodeByDex <119005046+CodeByDex@users.noreply.github.com>
2025-05-29 18:39:53 -0400

    added link to hawkbeat

--- data/eventDB.ts
+++ data/eventDB.ts
@@ -167,7 +167,7 @@ Reserved Seating Here: <a href='https://commerce.arryved.com/location/BDUsrPeg/m
     },
     {
         name: "Brewito Monday",
-        description: "During the June Brewito Monday event, Jonny Burritos will have a Pride-month themed menu, including rainbow rice in all burritos! While Jonny slings burritos, FBC will have a full bar open with beer, wine, cocktails, and n/a beverages! Plus enjoy some funky music by HawkBeat. ",
+        description: `During the June Brewito Monday event, Jonny Burritos will have a Pride-month themed menu, including rainbow rice in all burritos! While Jonny slings burritos, FBC will have a full bar open with beer, wine, cocktails, and n/a beverages! Plus enjoy some funky music by <a href="https://facebook.com/profile.php?id=61574639848750" target="_blank" class="underline">HawkBeat</a>. `,
         startDate: new Date(2025, 5, 16, 17, 0),
         endDate: new Date(2025, 5, 16, 20, 0),
         location: "Findlay Brewing Company - 213 E. Crawford St",
