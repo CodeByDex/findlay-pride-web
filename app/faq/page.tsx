d1aecf4494e80eb67340a94fc2c92e91edaae5e8
CodeByDex <119005046+CodeByDex@users.noreply.github.com>
2026-02-08 16:38:11 -0500

    updated site for 2026

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -17,14 +17,6 @@ export default function Home() {
                     <div className='response'></div>
                 </div> */}
                 <div>
-                    <div className='question'>I thought LGBTQ+ Spectrum of Findlay (Spectrum) and/or Kaleidoscope Youth Center (KYC) planned Pride in Findlay? </div>
-                    <div className='response'>
-                        Spectrum (and now/soon to be KYC) has historically led the effort for Pride events in Findlay, and they are still planning a “<Link href="https://www.facebook.com/share/p/197TNoZQnz/" target='_blank' className='underline'>Mini Pride</Link>” in June. 
-                        If you are interested in helping the community through charitable means please consider donating to KYC at: <Link href="https://kyc.app.neoncrm.com/forms/online-giving" target="_blank" className='underline'>https://kyc.app.neoncrm.com/forms/online-giving</Link>.
-                        This website's goal is to show appreciation for every business and organization that is creating an engaging and welcoming environment for Findlay citizens in June. 
-                    </div>
-                </div>
-                <div>
                     <div className='question'>What’s the catch?</div>
                     <div className='response'>No catch! We want to support businesses and organizations that support the community - simple as that! 
                         There are no financial obligations to be participate we just want to show appreciation for the support you are showing the Findlay community.  
@@ -32,7 +24,7 @@ export default function Home() {
                 </div>
                 <div>
                     <div className='question'>Who are you?</div>
-                    <div className='response'>We are a collaborative effort between a group of Findlay citizens and Spectrum.</div>
+                    <div className='response'>We are a group of Findlay citizens interested in making Findlay a better community for everyone!.</div>
                 </div>
                 <div>
                     <div className='question'>What information should I include in my Pride Month offering?</div>
@@ -47,10 +39,6 @@ export default function Home() {
                             <li>Information on if/how you plan promote so we can coordinate with you</li>
                         </ul>
                     </div>
-                </div>
-                <div>
-                    <div className='question'>What about Pride in the Park?</div>
-                    <div className='response'>We encourage you to check out the latest information regarding Pride in the Park from <Link href="https://www.facebook.com/lgbtqSpectrumofFindlay" target='_blank' className='underline'>Spectrum</Link> on their socials. The latest information we have from Spectrum is this event is postponed until 9/6. </div>
                 </div>
             </div>
         </main>
