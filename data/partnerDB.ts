e54ffd72cf169ea3a070d9ef220f845ba0ae7c0d
Ryan <119005046+CodeByDex@users.noreply.github.com>
2026-02-13 09:37:55 -0500

    Feature updates for partners and KYC GSA event (#50)
    * added partners back from 2025 until we begin 2026 outreach
    
    * listed KYC GSA event

--- data/partnerDB.ts
+++ data/partnerDB.ts
@@ -37,7 +37,147 @@ const partners: Partner[] = [
             instagram: undefined
         },
         highlight: false
-    }
+    },
+    {
+        name: "Kaleidoscope Youth Center",
+        imageURI: "/partnerLogo/kycLogo.png",
+        imageAlt: "Kaleidoscope Youth Center Logo",
+        organizerURI: "https://kycohio.org/",
+        socials: {
+            facebook: "https://facebook.com/KYCOhio/",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "Coffee Amici",
+        imageURI: "/partnerLogo/CALogo.png",
+        imageAlt: "Coffee Amici Logo",
+        organizerURI: "https://coffeeamici.com/",
+        socials: {
+            facebook: "https://facebook.com/coffeeamici",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "First Christian Church",
+        imageURI: "/partnerLogo/firstChristian.png",
+        imageAlt: "First Christian Church Logo",
+        organizerURI: "https://www.fccfindlay.org",
+        socials: {
+            facebook: "https://facebook.com/FindlayFCC",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "Jonny Burritos",
+        imageURI: "/partnerLogo/jbLogo.png",
+        imageAlt: "Jonny Burritos Logo",
+        organizerURI: "https://streetfoodfinder.com/jonnyburritos",
+        socials: {
+            facebook: "https://facebook.com/jonnyburritos",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "Findlay Wine Merchant",
+        imageURI: "/partnerLogo/WMLogo.png",
+        imageAlt: "Findlay Wine Merchant Logo",
+        organizerURI: "https://www.findlaywinemerchant.com",
+        socials: {
+            facebook: "https://facebook.com/findlaywinemerchant",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "Mooney Design",
+        imageURI: "/partnerLogo/mdLogo.png",
+        imageAlt: "Mooney Design Logo",
+        organizerURI: "https://mooneybdesign.com",
+        socials: {
+            facebook: undefined,
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "The Findlay Office",
+        imageURI: "/partnerLogo/tfoLogo.png",
+        imageAlt: "The Findlay Office Logo",
+        organizerURI: "https://thefindlayoffice.com",
+        socials: {
+            facebook: "https://facebook.com/thefindlayoffice",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "False Chord Brewing",
+        imageURI: "/partnerLogo/fcLogo.png",
+        imageAlt: "False Chord Brewing Logo",
+        organizerURI: "https://www.falsechordbrewing.com",
+        socials: {
+            facebook: "https://www.facebook.com/falsechordbrewing",
+        },
+        highlight: false
+    },
+    {
+        name: "Reading Grounds",
+        imageURI: "/partnerLogo/rgLogo.png",
+        imageAlt: "Reading Grounds Logo",
+        organizerURI: "https://readinggroundsohio.com/",
+        socials: {
+            facebook: "https://www.facebook.com/profile.php?id=61559814922888",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "Further Bikes",
+        imageURI: "/partnerLogo/fbLogo.png",
+        imageAlt: "Further Bikes Logo",
+        organizerURI: "",
+        socials: {
+            facebook: undefined,
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
+    {
+        name: "Findlay High School Genders and Sexualities Association",
+        imageURI: "/partnerLogo/fhsgsaLogo.png",
+        imageAlt: "Findlay High School Gender and Sexualities Association Logo",
+        organizerURI: "",
+        socials: {
+            facebook: "https://www.facebook.com/Findlay.High.School.GSA",
+            bluesky: undefined,
+            linkedIn: undefined,
+            instagram: undefined
+        },
+        highlight: false
+    },
 ]
 
 export default partners;
