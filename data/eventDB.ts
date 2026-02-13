7c2b689b25038017b93101653040fcefb2cbe4ce
CodeByDex <119005046+CodeByDex@users.noreply.github.com>
2026-02-13 09:35:48 -0500

    listed KYC GSA event

--- data/eventDB.ts
+++ data/eventDB.ts
@@ -4,7 +4,8 @@ const events: Event[] = [
 //    {
 //         name: "Event Title",
 //         description: `Event Description
-// Multiline Supported`,
+// Multiline Supported
+// <a href='https://some.url' class='underline' target='_blank'>URLs are supported too</a>`,
 //         startDate: new Date(2025, 5, 6, 14, 0),
 //         endDate: new Date(2025, 5, 29, 19, 0),
 //         location: "Some Location - Some Address",
@@ -33,6 +34,39 @@ const events: Event[] = [
         imageAlt: "A pair of eyes on a black background with the text 'Magestik's Nightmare on Main Street - United's 13th Annual Drag Show'",
         tags: [],
 },
+   {
+        name: "2026 GSA Spring Summit",
+        description: `The Ohio Gender and Sexuality Alliance (GSA) Network Summit, hosted by Kaleidoscope Youth Center and our community partners, is back! This in-person summit on advocacy, activism, and community building welcomes Ohio student GSA members ages 12–20 to Findlay on Saturday, March 14.
+
+        📝Registration is easy and FREE! → <a href='https://tinyurl.com/KYCGSASummit26' class='underline' target='_blank'>https://tinyurl.com/KYCGSASummit26</a>
+
+        Your GSA not registered with the Ohio GSA Network? Registering your school’s GSA takes just 5 minutes. It unlocks our monthly newsletter, GSA curriculum updates from us, networking opportunities, the annual summit, and more!
+
+        Register your GSA → <a href='https://www.kycohio.org/gsaregistration' class='underline' target='_blank'>https://www.kycohio.org/gsaregistration</a>
+
+        📍GSA Network Summit Details:
+Who: Ohio GSA members ages 12-20
+When: Saturday, March 14th, 11:00 am - 4:00 pm
+Where: Findlay-Hancock County Public Library
+Lindamood Room
+206 Broadway St, Findlay, Ohio 45840
+Lunch: Yes! Lunch will be provided from Main Street Deli & Arcade (<a href='https://www.facebook.com/msdarcade' class='underline' target='_blank'>https://www.facebook.com/msdarcade</a>)
+Cost: No cost! Registration is FREE and lunch is provided
+
+✨ Stay tuned for more info about this year’s Summit, including program and event highlights, keynote speaker announcement, and more!
+
+❓ Questions? Email <a href='mailto:education@kycohio.org' class='underline' target='_blank'>education@kycohio.org</a>`,
+        startDate: new Date(2026, 2, 14, 11, 0),
+        endDate: new Date(2026, 2, 14, 16, 0),
+        location: "Findlay-Hancock County Public Library - Lindamood Room, 206 Broadway St, Findlay, Ohio 45840",
+        organizer: "Kaleidoscope Youth Center",
+        organizerURI: "https://www.kycohio.org/",
+        eventURI: "https://www.facebook.com/share/1AmMowfPXQ/",
+        imageURI: "/event/gsaSpringSummit.png",
+        imageAlt: "Color design resembling magezine collage spelling out GSA Spring Summit along with the KYC and GSA Network logos",
+        tags: [],
+        //volunteerURI: "https://some.url/volunteer"
+    },
 ];
 
 export default events;
