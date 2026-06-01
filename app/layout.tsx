94f82ae0f02fd985a68dc3fc985ea13120ac33ad
Ryan <119005046+CodeByDex@users.noreply.github.com>
2026-06-01 19:38:35 -0400

    Added more partners and created passport page (#59)

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,6 +73,7 @@ export default function RootLayout({
           </div>
           <div className="grow"></div>
           <div><Link className="ml-5" href="/events">Events</Link></div>
+          <div><Link className="ml-5" href="/passport">Passport</Link></div>
           <div><Link className="ml-5" href="/volunteer">Volunteer</Link></div>
           <div><Link className="ml-5" href="/about">About</Link></div>
         </header>
